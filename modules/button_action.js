4cc4041494205cf852ff222071688edf49be86d2
sujavoonna <33873417+sujavoonna@users.noreply.github.com>
2017-11-28 15:01:07 -0500

    change

--- modules/button_action.js
+++ modules/button_action.js
@@ -74,18 +74,10 @@ exports.execute = (req, res) => {
         })
         .catch((error) => {
             if (error.code == 401) {
-                let fields = [];
-                //fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + caseId, short:false});
-                fields.push({title: "visit the URL to login", value: `https://${req.hostname}/login`});
-                let message = {
-                    text: "visit the URL to login",
-                    attachments: [
-                        {color: "#F2CF5B", fields: fields  }
-                    ]             
-                 }
+                
                // var url = req.body.payload;
-               res.json(message);
-               //res.send(`Visit the URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
+               //res.json(message);
+               res.send(`Visit the URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
                 //console.log('url'+url);
                 //res.send(url);
             } else {
