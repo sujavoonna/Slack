36a172f0658984688f93a42f19d1d664447caa3b
sujavoonna <33873417+sujavoonna@users.noreply.github.com>
2017-11-28 11:52:51 -0500

    change

--- modules/button_action.js
+++ modules/button_action.js
@@ -75,7 +75,7 @@ exports.execute = (req, res) => {
         .catch((error) => {
             if (error.code == 401) {
                 let fields = [];
-                fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + caseId, short:false});
+                //fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + caseId, short:false});
                 fields.push({title: "visit the URL to login", value: "https://${req.hostname}/login/" + slackUserId});
                 let message = {
                     text: "visit the URL to login",
