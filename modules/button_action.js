d354a6418110627321a4db26d90efbae98e6e730
sujavoonna <33873417+sujavoonna@users.noreply.github.com>
2017-12-11 13:49:36 -0500

    chnage

--- modules/button_action.js
+++ modules/button_action.js
@@ -73,7 +73,8 @@ exports.execute = (req, res) => {
                                
                     //console.log(cases.requestSFUser+user);
                     console.log(data+"userjson");
-                    console.log(data[0].Message+"Message");
+                    console.log(data.Message[0]+"Message1");
+                    console.log(data.requestSFUser.Type+data.requestSFUser.SlackName+"Message2");
                
                 
                 let fields = [];
