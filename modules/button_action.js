c9107e5e48214014e8b2912445ac45a038ba9f90
sujavoonna <33873417+sujavoonna@users.noreply.github.com>
2017-12-11 12:27:09 -0500

    chnage

--- modules/button_action.js
+++ modules/button_action.js
@@ -72,7 +72,7 @@ exports.execute = (req, res) => {
                 let cases = JSON.parse(data).records;
                
                     //console.log(cases.requestSFUser+user);
-                    console.log(JSON.stringify(data)+user);
+                    console.log(JSON.stringify(data)+"user");
                
                 
                 let fields = [];
