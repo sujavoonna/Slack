4e527cb749d0b6b4ce358c75dbebea9f182c2d0c
sujavoonna <33873417+sujavoonna@users.noreply.github.com>
2017-12-19 11:13:08 -0500

    chnage

--- modules/button_action.js
+++ modules/button_action.js
@@ -75,10 +75,10 @@ exports.execute = (req, res) => {
                 {statusMessage =(caseId).bold()+ " has already been claimed by:"+casereturnInfo.oldCaseOwner.Name}; */
                 
                 let fields = [];
-                if(!casereturnInfo.Success) 
-                fields.push({title: caseId, value: "has already been claimed by:"+casereturnInfo.oldCaseOwner.Name, short:false});
-                else
-                fields.push({title: caseId, value: "Case's owner  has now been claimed by :"+casereturnInfo.requestSFUser.Name, short:false});
+              //  if(!casereturnInfo.Success) 
+               // fields.push({title: caseId, value: "has already been claimed by:"+casereturnInfo.oldCaseOwner.Name, short:false});
+              //  else
+              //  fields.push({title: caseId, value: "Case's owner  has now been claimed by :"+casereturnInfo.requestSFUser.Name, short:false});
                 fields.push({title: "Subject :", value: casereturnInfo.subject, short:false}); 
                // fields.push({title: "oldOwnerId", value: oldownerId, short:false});
                // fields.push({title: "newOwnerId", value: userId, short:false});
