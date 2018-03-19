6629cb8ae802a8fba98cb53c01182d1b54abe433
sujavoonna <33873417+sujavoonna@users.noreply.github.com>
2018-03-19 16:42:50 -0400

    chnage

--- modules/button_action.js
+++ modules/button_action.js
@@ -19,7 +19,7 @@ exports.execute = (req, res) => {
 	
     console.log('---message is name' + actionJSONPayload.user.name+'id'+actionJSONPayload.user.id);
   
-   console.log('before'+actionJSONPayload);
+   console.log('before'+req.body.payload);
     
           
 	//**********************************************************
